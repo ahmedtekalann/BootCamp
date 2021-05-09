596e27fc5ea23f172e871b9d2f11f8414753e861
Ahmed TEKALAN <ahmettekalan@gmail.com>
2021-05-09 17:41:15 +0300

    Spring boot in corporate architecture

---
+++ f_northwind/src/test/java/kodlamaio/f_northwind/FNorthwindApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlamaio.f_northwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FNorthwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
