c7285d623a693b43b6e3831edf2f42bc6403d9d4
Ahmed TEKALAN <ahmettekalan@gmail.com>
2021-05-10 04:00:58 +0300

    deleted.

--- f_northwind/src/main/java/kodlamaio/f_northwind/business/abstracts/ProductService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.f_northwind.business.abstracts;
-
-import java.util.List;
-
-import kodlamaio.f_northwind.entities.Product;
-
-public interface ProductService {
-     List<Product> getAll();
-}
--- f_northwind/src/main/java/kodlamaio/f_northwind/dataAccess/abstracts/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.f_northwind.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.f_northwind.entities.Product;
-
-public interface ProductDao extends JpaRepository<Product,Integer>{
-
-}
